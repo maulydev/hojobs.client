43aba767d710bfdad6bdf24927c9e9dd44445e90
maulydev <mauly.dev@gmail.com>
2023-10-14 05:21:57 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    // baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://hojobs.onrender.com/api",
 });
 
 export default instance;
